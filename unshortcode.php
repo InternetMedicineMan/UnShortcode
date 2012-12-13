1155247423344d2ec0ebfb02e0858f5c47d4941b
Jason Diehl <jason.diehl@turbowebs.com>
2012-12-13 02:29:34 -0600

    Found bug with :visited style, update to 0.8

--- unshortcode.php
+++ unshortcode.php
@@ -125,7 +125,7 @@ class BMLUnshortcode{
                     <div style="padding:0 15px 0 15px;">
                         <label for="button_text"><span style="display:inline-block;width: 80px;text-align: right;padding-right: 5px;">Text</span><input type="text" id="button_text" style="width: 360px;margin-top: 5px;" /></label><br />
                         <label for="button_link"><span style="display:inline-block;width: 80px;text-align: right;padding-right: 5px;">URL</span><input type="text" id="button_link" value="http://" style="width: 360px;margin-top: 5px;" /></label><br />
-                        <label for="button_title"><span style="display:inline-block;width: 80px;text-align: right;padding-right: 5px;">Title</span><input type="text" id="button_title" style="width: 360px;margin-top: 5px;" /></label>
+                        <label for="button_title"><span style="display:inline-block;width: 80px;text-align: right;padding-right: 5px;">Tooltip</span><input type="text" id="button_title" style="width: 360px;margin-top: 5px;" /></label>
                     </div>
                     <div style="padding:15px 15px 15px 100px;">
                         <input type="button" class="button-primary" value="Insert Button" onclick="InsertButton();"/>&nbsp;&nbsp;&nbsp;
